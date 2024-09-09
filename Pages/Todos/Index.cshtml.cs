9fcf672bb638c58dadb211332d22776d1c335780
mikepreston17 <michael.n.preston@gmail.com>
2024-09-09 02:14:13 -0500

    rm todostatus

--- Pages/Todos/Index.cshtml.cs
+++ Pages/Todos/Index.cshtml.cs
@@ -85,11 +85,11 @@ public class TodosRepository : ITodosRepository
         throw new NotImplementedException();
     }
 
-    public async Task<int> Create(params Todo[] model)
-    {
-        // return await InsertRow(model.First());
-        return default;
-    }
+    // public async Task<int> Create(params Todo[] model)
+    // {
+    //     // return await InsertRow(model.First());
+    //     return default;
+    // }
 
     public Task Update(int id, Todo model)
     {
@@ -193,27 +193,27 @@ public class TodosRepository : ITodosRepository
 //     {
 //     }
 // }
-
-public class TodoStatus : Enumeration
-{
-    public static TodoStatus Done = new TodoStatus(1, nameof(Done));
-    public static TodoStatus Pending = new TodoStatus(2, nameof(Pending));
-    public static TodoStatus WIP = new TodoStatus(3, nameof(WIP));
-    public static TodoStatus Postponed = new TodoStatus(4, nameof(Postponed));
-    public static TodoStatus Unknown = new TodoStatus(5, nameof(Unknown));
-
-    public TodoStatus(int id, string name) : base(id, name)
-    {
-    }
-
-    public static implicit operator TodoStatus(string status)
-    {
-        if (status.IsEmpty())
-            return Unknown;
-        var
-            found = TodoStatus
-                .GetAll<TodoStatus>()
-                .SingleOrDefault(x => x.Name.Equals(status, StringComparison.CurrentCultureIgnoreCase));
-        return found;
-    }
-}
+//
+// public class TodoStatus : Enumeration
+// {
+//     public static TodoStatus Done = new TodoStatus(1, nameof(Done));
+//     public static TodoStatus Pending = new TodoStatus(2, nameof(Pending));
+//     public static TodoStatus WIP = new TodoStatus(3, nameof(WIP));
+//     public static TodoStatus Postponed = new TodoStatus(4, nameof(Postponed));
+//     public static TodoStatus Unknown = new TodoStatus(5, nameof(Unknown));
+//
+//     public TodoStatus(int id, string name) : base(id, name)
+//     {
+//     }
+//
+//     public static implicit operator TodoStatus(string status)
+//     {
+//         if (status.IsEmpty())
+//             return Unknown;
+//         var
+//             found = TodoStatus
+//                 .GetAll<TodoStatus>()
+//                 .SingleOrDefault(x => x.Name.Equals(status, StringComparison.CurrentCultureIgnoreCase));
+//         return found;
+//     }
+// }
