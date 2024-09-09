351dac0336b892085fa1d20b4c931931056359c2
mikepreston17 <michael.n.preston@gmail.com>
2024-09-09 18:40:23 -0500

    organized sweet  alerts to own file

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,22 +2,5 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-// sweetalerts
-// credit: https://khalidabuhakmeh.com/confirmation-dialogs-with-htmx-and-sweetalert
 
-document.body.addEventListener('htmx:confirm', function (evt) {
-    if (evt.target.matches("[confirm-with-sweet-alert='true']")) {
-        evt.preventDefault();
-        swal({
-            title: "Are you sure?",
-            text: "Are you sure you are sure?",
-            icon: "warning",
-            buttons: true,
-            dangerMode: true,
-        }).then((confirmed) => {
-            if (confirmed) {
-                evt.detail.issueRequest();
-            }
-        });
-    }
-});
+require('./sweet_alerts.js')
