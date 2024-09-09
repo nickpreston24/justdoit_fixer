1ea4b6af6416931aa3b868cbb1f9b662ca1dd386
mikepreston17 <michael.n.preston@gmail.com>
2024-09-09 01:21:14 -0500

    moved repo to .cached for safekeeping

--- Pages/Todos/Foo.cs
+++
@@ -1,5 +0,0 @@
-namespace justdoit_fixer.Models;
-
-class Foo
-{
-}
--- Pages/Todos/Index.cshtml.cs
+++ Pages/Todos/Index.cshtml.cs
@@ -1,54 +1,11 @@
-using System.Diagnostics;
-using System.Runtime.CompilerServices;
-using Dapper;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace justdoit_fixer.Pages.Todos;
 
 public class Index : PageModel
 {
-    private readonly ITodosRepository todo_repo;
-    private bool debug;
-
-    public Index(ITodosRepository todosRepository)
-    {
-        this.todo_repo = todosRepository;
-    }
-
     public void OnGet()
     {
-        this.debug = true;
-    }
-
-    public async Task<IActionResult> OnGetAllTodos(string search_term, [CallerMemberName] string name = "")
-    {
-        if (debug) Console.WriteLine($"{name}:{search_term}");
-        Stopwatch watch = Stopwatch.StartNew();
-        watch.Stop();
-        var elapsed = watch.Elapsed;
-        var all_todos = await todo_repo.GetAll();
-        return Partial("_TodoTable", all_todos);
-    }
-
-    public async Task<IActionResult> OnGetTimeElapsed(string search_term, [CallerMemberName] string name = "")
-    {
-        try
-        {
-            if (debug) Console.WriteLine($"{name}:{search_term}");
-            Stopwatch watch = Stopwatch.StartNew();
-            using var connection = SqlConnections.CreateConnection();
-            var time = (await connection.QueryAsync<TimeElapsed>(@"select id from TimeElapsed")).ToList();
-            watch.Stop();
-            var elapsed = watch.Elapsed;
-            // return Content($"(mysql view call) total {time.Count} took {elapsed.Milliseconds} ms");
-
-            return Partial("_TimeElapsedTable", time);
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e);
-            throw;
-        }
+        
     }
 }
