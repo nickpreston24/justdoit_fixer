1466dcc6acb6fbcc2c4fa97a22ef2d6a3c15b871
mikepreston17 <michael.n.preston@gmail.com>
2024-09-09 05:07:48 -0500

    fixed dll name

--- Dockerfile
+++ Dockerfile
@@ -29,4 +29,4 @@ RUN dotnet publish -c Release -o out
 FROM mcr.microsoft.com/dotnet/aspnet:8.0
 WORKDIR /App
 COPY --from=build-env /App/out .
-ENTRYPOINT ["dotnet", "asp_containerized.dll"]
+ENTRYPOINT ["dotnet", "justdoit_fixer.dll"]
--- publish.sh
+++ publish.sh
@@ -1,1 +1,2 @@
-dotnet publish --runtime alpine-x64 -c Release --self-contained true -o ./publish
+dotnet publish -c Release --self-contained true -o ./publish
+# --runtime alpine-x64   ## <-- add this when you want to publish and run as a docker image.
