d1fd6c6b6eedc6aa2afc768b1b88868301861fbe
mikepreston17 <michael.n.preston@gmail.com>
2024-09-10 00:39:50 -0500

    ++ Pine samples

---
+++ Pages/Examples/PinesAccordion.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace justdoit_fixer.Pages;
+
+public class PinesExamples : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
---
+++ Pages/Examples/PinesToast.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace justdoit_fixer.Pages;
+
+public class PinesToast : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
