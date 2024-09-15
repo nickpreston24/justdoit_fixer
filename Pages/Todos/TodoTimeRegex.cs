11a94a3d73a7ae26412bdd51c3b77431938d2248
mikepreston17 <michael.n.preston@gmail.com>
2024-09-14 21:50:12 -0500

    neutered regexenumbase inheritance

--- Pages/Todos/TodoTimeRegex.cs
+++ Pages/Todos/TodoTimeRegex.cs
@@ -4,16 +4,16 @@ using CodeMechanic.Types;
 namespace justdoit.Models;
 
 //
-public class TodoTimeRegex : RegexEnumBase
-{
-    // public static TodoTimeRegex Basic = new TodoTimeRegex(1, nameof(Basic),
-    //     @"(?<value>\d+)\s*?(?<unit>days?|months?|hours?minutes?|w|d|h|hrs?|mins?)", "https://regex101.com/r/7BbzEK/1");
-
+// public class TodoTimeRegex : RegexEnumBase
+// {
+//     // public static TodoTimeRegex Basic = new TodoTimeRegex(1, nameof(Basic),
+//     //     @"(?<value>\d+)\s*?(?<unit>days?|months?|hours?minutes?|w|d|h|hrs?|mins?)", "https://regex101.com/r/7BbzEK/1");
 //
-    protected TodoTimeRegex(int id, string name, string pattern, string uri = "") : base(id, name, pattern, uri)
-    {
-    }
-}
+// //
+//     protected TodoTimeRegex(int id, string name, string pattern, string uri = "") : base(id, name, pattern, uri)
+//     {
+//     }
+// }
 
 public class TodoTime
 {
@@ -26,18 +26,19 @@ public class TodoTime
     public int months { get; set; } = 0;
 }
 
-public class RegexEnumBase : Enumeration
-{
-    protected RegexEnumBase(int id, string name, string pattern, string uri = "") : base(id, name)
-    {
-        Pattern = pattern;
-        CompiledRegex =
-            new Regex(pattern, RegexOptions.Compiled | RegexOptions.IgnoreCase);
-        this.uri = uri;
-    }
-
-    public string uri { get; set; } = string.Empty;
-
-    public Regex CompiledRegex { get; set; }
-    public string Pattern { get; set; }
-}
+//
+// public class RegexEnumBase : Enumeration
+// {
+//     protected RegexEnumBase(int id, string name, string pattern, string uri = "") : base(id, name)
+//     {
+//         Pattern = pattern;
+//         CompiledRegex =
+//             new Regex(pattern, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+//         this.uri = uri;
+//     }
+//
+//     public string uri { get; set; } = string.Empty;
+//
+//     public Regex CompiledRegex { get; set; }
+//     public string Pattern { get; set; }
+// }
